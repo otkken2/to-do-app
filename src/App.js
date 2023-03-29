cd20d208e014d338d73587750871124a3cc5c533
otkken2 <lovelovezamakko2@gmail.com>
2023-03-30 03:46:22 +0900

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputText,setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [doneList, setDoneList] = useState([]);
+
+  const handleChangeInputText = (e) => {
+    console.log(e.target.value)
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("[...todos,inputText]");
+    console.log([...todos,inputText]);
+    setTodos([...todos,inputText]);
+    setInputText('');
+  }
+
+  const handleClickDelete = (el) => {
+    // console.log('削除！');
+    setTodos(prevTodos => {
+      return prevTodos.filter(eachTodo => eachTodo !== el);
+    });
+  };
+
+  const handleClickDone = (el) => {
+    handleClickDelete(el);
+    setDoneList([...doneList,el]);
+  };
+
+  const handleClickReturn = (el) => {
+    setDoneList(prevDoneList => prevDoneList.filter(eachDone => eachDone !== el));
+    setTodos(prevTodos => [...prevTodos,el]);
+  }
+  console.log("todos");
+  console.log(todos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen w-screen tex-center flex flex-col">
+      <h1>TodoApp</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='flex mb-10 mx-auto px-20 justify-center'>
+          <label className='w-14 self-center'>追加：</label>
+          <input value={inputText} type="text" onChange={(e)=> handleChangeInputText(e)} className='w-1/2 p-5 border h-10 border-solid border-cyan-950 rounded-full'/>
+          <button type="submit"className='border border-solis w-22 rounded-full'>追加する</button>
+        </div>
+        <p>やることリスト:</p>
+        { todos.length ?
+          <div className='mx-auto w-1/2 mb-5'>
+            <ul>
+              {
+                todos.map((eachTodo,index) => {
+                  return (
+                    <li key={index} className='flex w-full justify-between'>
+                      {eachTodo}
+                      <div className='flex'>
+                        <p className='rounded-full w-14 border-2 border-solid  ml-10' onClick={()=> handleClickDelete(eachTodo)}>削除</p>
+                        <p className='rounded-full w-14 border-2 border-solid  ml-5' onClick={()=> handleClickDone(eachTodo)}>完了</p>
+                      </div>
+                    </li>
+                  );
+                })
+              }
+            </ul>
+          </div>
+          :
+          <p className='mb-5'>特になし</p>
+        }
+        <p>完了リスト:</p>
+        { doneList.length ?
+        <div className='mx-auto w-1/2 mb-5'>
+          <ul>
+            {
+              doneList.map(eachDone => {
+                return (
+                  <li className='flex justify-between'>
+                    {eachDone}
+                    <div className='flex'>
+                      <p className='rounded-full w-14 border-2 border-solid  ml-10' onClick={()=> handleClickDelete(eachDone)}>削除</p>
+                      <p className='rounded-full w-14 border-2 border-solid  ml-5' onClick={()=> handleClickReturn(eachDone)}>戻す</p>
+                    </div>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+        :
+        <p>まだ無し</p>
+        }
+      </form>
     </div>
   );
 }
